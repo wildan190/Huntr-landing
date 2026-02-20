065f561caa6689e01cbbecb2bb620815b69c7799
Huntr <huntrconnect@gmail.com>
2026-02-20 19:37:25 +0000

    btn switch language nya buatlah transparent juga dan jangan berikan bord

--- src/components/language-toggle.tsx
+++ src/components/language-toggle.tsx
@@ -24,7 +24,7 @@ export function LanguageToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
+        <Button variant="ghost" size="icon">
           <Globe className="h-[1.2rem] w-[1.2rem]" />
           <span className="sr-only">Toggle language</span>
         </Button>
