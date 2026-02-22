23fa539129f34cc5b2403f21c16d37fe9ba0007d
Huntr <huntrconnect@gmail.com>
2026-02-22 06:56:44 +0000

    jangan ada yg menempel di border samping juga

--- src/app/our-team/page.tsx
+++ src/app/our-team/page.tsx
@@ -57,8 +57,8 @@ export default function OurTeamPage() {
               {teamMembers.map((member, index) => {
                 const avatarImage = PlaceHolderImages.find(p => p.id === member.avatarId);
                 return (
-                  <AnimateOnScroll key={member.id} className="fade-in zoom-in-95 duration-700" style={{ animationDelay: `${index * 100}ms` }}>
-                    <Card className="flex flex-col md:flex-row md:items-center overflow-hidden">
+                  <AnimateOnScroll key={member.id} className="fade-in zoom-in-95 duration-700">
+                    <Card className="flex flex-col md:grid md:grid-cols-[auto_1fr] md:items-center">
                       
                       {/* Image */}
                       <div className="relative w-full md:w-64 flex-shrink-0 h-80">
@@ -74,7 +74,7 @@ export default function OurTeamPage() {
                       </div>
 
                       {/* Content */}
-                      <div className="flex-1 p-6 sm:p-8">
+                      <div className="p-6 sm:p-8">
                         <div>
                           <CardTitle className="font-headline text-2xl">{member.name}</CardTitle>
                           <p className="text-primary font-medium mt-1">{member.title}</p>
